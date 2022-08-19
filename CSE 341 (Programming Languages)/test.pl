5cd803312e487659492ff18966785d0c51e89470
huseyin-yildiz <huseyin3421___@hotmail.com>
2022-08-19 15:03:50 +0300

    PL removed

--- CSE 341 (Programming Languages)/test.pl
+++
@@ -1,7 +0,0 @@
-even(X) :- mod(X,2) =:= 0.
-
-
-%even_between(A,B,X) :- X is (A+1) , X = B, even(X)   .
-%even_between(A,B,X) :- A<X , B>=X, even(X), A1 is A+1,even_between(A1,B,X) .
-
-even_between(A,B,X) :- between(A, B, X), even(X) .
